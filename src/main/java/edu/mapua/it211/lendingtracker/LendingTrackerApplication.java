2f3f56ee8dda5e4480a3f7f2f89b2bf3546fe6ae
Antonio Oro Dungao <antonioorodungao@yahoo.com>
2023-09-30 19:51:42 +0300

    Initial commit

---
+++ src/main/java/edu/mapua/it211/lendingtracker/LendingTrackerApplication.java
@@ -0,0 +1,13 @@
+package edu.mapua.it211.lendingtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LendingTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LendingTrackerApplication.class, args);
+	}
+
+}
